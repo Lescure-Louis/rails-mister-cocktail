f185117b03ee8a674c70adf19c688ddeb1f5bfd3
Louis Lescure <louis.lescure@mail.mcgill.ca>
2019-11-15 16:49:51 +0100

    did some stuff

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
 
 
